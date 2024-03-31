8946174e253744f653536df04a1864aecc7bf05f
Bazerche21 <149426550+Bazerche21@users.noreply.github.com>
2024-03-31 20:38:22 +0200

    Add files via upload

---
+++ Homework#1 - Class2 Part1/Employeee/Program.cs
@@ -0,0 +1,9 @@
+﻿using Employee.Domain.Models;
+
+Manager manager = new("Laze Lazarevski", "M0247", 5000, 1000);
+manager.DisplayInfo();
+
+Console.WriteLine();
+
+Programmer programmer = new("Meri Mijovska", "F0488", 4500, 15);
+programmer.DisplayInfo();
