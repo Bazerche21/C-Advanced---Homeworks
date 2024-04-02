8738e8658b890ef2422b067114171587d6817192
Bazerche21 <149426550+Bazerche21@users.noreply.github.com>
2024-04-02 17:37:02 +0200

    Delete Homework#1 - Class2 Part1 directory

--- Homework#1 - Class2 Part1/Employeee/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Employee.Domain.Models;
-
-Manager manager = new("Laze Lazarevski", "M0247", 5000, 1000);
-manager.DisplayInfo();
-
-Console.WriteLine();
-
-Programmer programmer = new("Meri Mijovska", "F0488", 4500, 15);
-programmer.DisplayInfo();
