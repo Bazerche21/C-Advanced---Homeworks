b295c47ff19971ac3d2fe7b914cafb7c2f731134
Bazerche21 <matejbazerkoski@gmail.com>
2024-04-24 17:34:12 +0200

    Homework#5 - Class 9 uploaded

---
+++ Homework#5 - Class9/Class9.FileStream/Solution1/FileStream/Program.cs
@@ -0,0 +1,41 @@
+﻿string appPath = @"..\..\..\";
+string folderPath = appPath + @"myFolder\";
+string filePath = folderPath + @"names.txt";
+
+Directory.CreateDirectory(folderPath);
+
+Console.WriteLine("Enter names (one per line). Type 'done' when finished:");
+using (StreamWriter writer = new StreamWriter(filePath, true))
+{
+    string name;
+    while ((name = Console.ReadLine()) != "done")
+    {
+        writer.WriteLine(name);
+    }
+}
+
+FilterNamesByLetter(filePath);
+Console.WriteLine("Task completed successfully.");
+
+void FilterNamesByLetter(string filePath)
+{
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    foreach (char letter in alphabet)
+    {
+        string outputFilePath = Path.Combine(@"Files", $"namesStartingWith_{letter}.txt");
+        var namesStartingWithLetter = File.ReadLines(filePath)
+                                           .Where(name => name.StartsWith(letter.ToString()))
+                                           .ToList();
+
+        if (namesStartingWithLetter.Any())
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath, true))
+            {
+                foreach (var name in namesStartingWithLetter)
+                {
+                    writer.WriteLine(name);
+                }
+            }
+        }
+    }
+}
