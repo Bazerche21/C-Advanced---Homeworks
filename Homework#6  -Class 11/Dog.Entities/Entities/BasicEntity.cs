db22006ba747213b417b661a8934cbe65c02c324
Bazerche21 <matejbazerkoski@gmail.com>
2024-05-04 11:38:56 +0200

    Homework #6 - Class 11

---
+++ Homework#6  -Class 11/Dog.Entities/Entities/BasicEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Dogs.Domain.Entities
+{
+    public abstract class BasicEntity
+    {
+        public int Id {  get; set; }
+        public abstract string getInfo();
+    }
+}
---
+++ Homework#6  -Class 11/Dog.Entities/Entities/Dog.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dogs.Domain.Entities
+{
+    public class Dog : BasicEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public Dog(string name, int age, string color)
+        {
+            Name = name;
+            Age = age;
+            Color = color;
+        }
+        public override string getInfo()
+        {
+            return $"{Name} is {Age} years old and has {Color}!";
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static Dog FromString(string json)
+        {
+            return JsonConvert.DeserializeObject<Dog>(json);
+        }
+        }
+    }
